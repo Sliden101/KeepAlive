1781edb3fec72af7df4d03b0f5c006b9ddaf595b
Orphan Obliterator <57210425+Urtext69@users.noreply.github.com>
2021-02-09 12:46:24 +0700

    Create keepalive.js

---
+++ keepalive.js
@@ -0,0 +1,13 @@
+// server.js
+// where your node app starts
+// init project
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/views/index.html");
+});
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
